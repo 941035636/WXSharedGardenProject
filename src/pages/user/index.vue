a0373e96c20fef3800a90af4133b4a6c216228e7
WIN-B6RE0PP79DP\Administrator <13371031274@163.com>
2024-04-12 12:46:55 +0800

    搭建我的模块分包

--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -1,9 +1,43 @@
 <template>
-	<view>
-        我的
+	<view class="w-[750rpx]">
+        <view class="w-[750rpx] h-[100rpx] textClass" @click="goUserAdmin">用户管理</view>
+        <view class="w-[750rpx] h-[100rpx] textClass" @click="goOrderForm">商城订单</view>
+        <view class="w-[750rpx] h-[100rpx] textClass" @click="goResourceAdmin">资源管理</view>
+        <view class="w-[750rpx] h-[100rpx] textClass" @click="goBrandAdmin">品牌管理</view>
+        <view class="w-[750rpx] h-[100rpx] textClass" @click="goSafetyAdmin">安全管理</view>
     </view>
 </template>
 <script setup lang="ts">
+const goUserAdmin = () => {
+    uni.navigateTo({
+        url: '/minePackage/pages/userAdmin'
+    });
+};
+const goOrderForm = () => {
+    uni.navigateTo({
+        url: '/minePackage/pages/orderForm'
+    });
+};
+const goResourceAdmin = () => {
+    uni.navigateTo({
+        url: '/minePackage/pages/resourceAdmin'
+    });
+};
+const goBrandAdmin = () => {
+    uni.navigateTo({
+        url: '/minePackage/pages/brandAdmin'
+    });
+};
+const goSafetyAdmin = () => {
+    uni.navigateTo({
+        url: '/minePackage/pages/safetyAdmin'
+    });
+};
 </script>
 <style lang="scss" scoped>
+.textClass{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
 </style>
---
+++ src/sharePackage/pages/cropland.vue
@@ -0,0 +1,9 @@
+<template>
+	<view>
+        农⽥认种
+    </view>
+</template>
+<script setup lang="ts">
+</script>
+<style lang="scss" scoped>
+</style>
